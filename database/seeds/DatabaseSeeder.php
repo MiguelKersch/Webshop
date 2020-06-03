98d143e868c6a9a1a605c05d6525b057c2054e3b
MiguelKersch <99051637@mydavinci.nl>
2020-06-03 13:34:16 +0200

    added a seeder for category

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        for ($category = 1; $category < 6; $category++) {
+            DB::table('category')->insert([
+                'name' => 'category' . $category,
+                'description' => 'description'
+            ]);
+        }
     }
 }
