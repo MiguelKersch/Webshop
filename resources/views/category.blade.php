574f6d6b2cc0008611ed2284d5fde08e3c2ea997
MiguelKersch <99051637@mydavinci.nl>
2020-06-10 10:10:12 +0200

    made some changes to the view

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,23 +1,28 @@
 @include('layouts.head')
-@include('layouts.navbar')
 
-<h1>Category`s</h1>
-<table class="table table-striped">
-    <thead class="bg-dark text-light">
-        <tr scope="row">
-            <td scope="col">Id</td>
-            <td scope="col">Name</td>
-            <td scope="col">Description</td>
-        </tr>
-    </thead>
-    <tbody>
-    @foreach($categorys as $category)
-    <tr scope="row">
-        <td>{{ $category->id }}</td>
-        <td>{{ $category->name }}</td>
-        <td>{{ $category->description }}</td>
-        <button><a class="text-light nav-link" href="{{ url('/category/product?'. $category->id) }}">Category</a></button>
-    </tr>
-    @endforeach
-    </tbody>
-</table>
+<body>
+    @include('layouts.navbar')
+
+    <h1>Category`s</h1>
+    <table class="table table-striped">
+        <thead class="bg-dark text-light">
+            <tr scope="row">
+                <td scope="col">Id</td>
+                <td scope="col">Name</td>
+                <td scope="col">Description</td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($categorys as $category)
+            <tr scope="row">
+                <td>{{ $category->id }}</td>
+                <td>{{ $category->name }}</td>
+                <td>{{ $category->description }}</td>
+                <td><a class="text-dark nav-link" href="/product/{{ $category->id }}"><i class="fas fa-arrow-right"></i></a></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+</body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @include('layouts.head')
 <body>
     @include('layouts.navbar')
-    
+    <h1>Webshop</h1>
 </body>
 
 </html>
